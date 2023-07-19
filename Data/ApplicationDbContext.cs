8a20fc74a8f2b927ec34b2d5272900477a8e11c2
Lisandro Martin <69371212+Sgtdoakes@users.noreply.github.com>
2023-07-19 15:21:34 -0300

    Add files via upload

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,33 @@
+﻿using BulkyWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+                
+        }
+            
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "SciFi", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "History", DisplayOrder = 3 }
+                );
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .EnableSensitiveDataLogging() // Enable sensitive data logging
+                .UseSqlServer("Server=.;Database=Bulky;Trusted_Connection=True;TrustServerCertificate=True");
+        }
+
+    }
+}
